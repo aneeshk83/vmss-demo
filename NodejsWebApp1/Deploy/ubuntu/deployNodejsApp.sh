979dd5148c14dfe225f25b1b98834c11bed3b951
anubhav-beep <70649851+anubhav-beep@users.noreply.github.com>
2021-03-01 13:39:35 +0530

    Update deployNodejsApp.sh

--- NodejsWebApp1/Deploy/ubuntu/deployNodejsApp.sh
+++ NodejsWebApp1/Deploy/ubuntu/deployNodejsApp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs
 sudo apt-get update
